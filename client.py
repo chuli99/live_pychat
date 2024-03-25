d6f58430f6aa61ed9db131d335f480954d8ede28
chuli99 <juliancastillo0399@gmail.com>
2024-03-25 20:25:29 -0300

    #2 using threading in client

--- client.py
+++ client.py
@@ -1,17 +1,34 @@
 import socket
+import threading
 
 HOST, PORT = "localhost", 5555
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    print(sock.recv(1024).decode())
+def recieve_messages(sock):
+    while True:
+        message = sock.recv(1024).decode()
+        if not message:
+            break
+        print(message)
 
+def send_messages(sock):
     while True:
         message = input("Enter message: ")
         sock.sendall(message.encode())
         if message.lower() == "exit":
             break
 
-        print("Esperando mensaje de otro cliente")
-        recieve_message = sock.recv(1024).decode()
-        print("Server saiys:",recieve_message)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    # Mensaje de bienvenida del server
+    print(sock.recv(1024).decode())
+
+    #Inicializo hilo para recibir mensajes
+    recieve_msg_thread = threading.Thread(target=recieve_messages,args=(sock,))
+    recieve_msg_thread.start()
+
+    send_msg_thread = threading.Thread(target=send_messages,args=(sock,))
+    send_msg_thread.start()
+    
+    #Esperar que ambos hilos terminen
+    recieve_msg_thread.join() 
+    send_msg_thread.join()
