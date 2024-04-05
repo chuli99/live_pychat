a6ebdd1ca31ba2cf072a32f0571bb3046252eff1
chuli99 <juliancastillo0399@gmail.com>
2024-04-05 12:36:06 -0300

    client working

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
 import socket
 import threading
+from subprocess import Popen,PIPE
 
-HOST, PORT = "localhost", 5555
 
-def recieve_messages(sock):
+HOST, PORT = "localhost", 5554
+
+def receive_messages(sock):
     while True:
-        message = sock.recv(1024).decode()
+        message = sock.recv(512).decode()
         if not message:
             break
         print(message)
+        
 
 def send_messages(sock):
     while True:
-        message = input("Enter message: ")
+        message = input()
         sock.sendall(message.encode())
         if message.lower() == "exit":
             break
@@ -20,15 +23,16 @@ def send_messages(sock):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
     # Mensaje de bienvenida del server
-    print(sock.recv(1024).decode())
+    print(sock.recv(512).decode())
 
     #Inicializo hilo para recibir mensajes
-    recieve_msg_thread = threading.Thread(target=recieve_messages,args=(sock,))
-    recieve_msg_thread.start()
-
+    recieve_msg_thread = threading.Thread(target=receive_messages,args=(sock,))
     send_msg_thread = threading.Thread(target=send_messages,args=(sock,))
+
     send_msg_thread.start()
-    
+    recieve_msg_thread.start()
     #Esperar que ambos hilos terminen
     recieve_msg_thread.join() 
     send_msg_thread.join()
+
+    
