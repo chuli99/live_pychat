24da2740849cd0041118c84b6c84cec1a98236fe
chuli99 <juliancastillo0399@gmail.com>
2024-04-08 12:59:25 -0300

    menu options added

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import socket
 import threading
+import menu
 from subprocess import Popen,PIPE
 
 
-HOST, PORT = "localhost", 5554
+HOST, PORT = "localhost", 5555
 
 def receive_messages(sock):
     while True:
@@ -24,15 +25,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
     # Mensaje de bienvenida del server
     print(sock.recv(512).decode())
+    while True:    
+        menu.show_menu()
+        option = input("Opcion:")
+        if option == "1":
+            print("Chat P2P is not available")
+        if option == "2":
+            menu.show_rooms()
+            room = input("Select rooms: <name_room>")
 
-    #Inicializo hilo para recibir mensajes
-    recieve_msg_thread = threading.Thread(target=receive_messages,args=(sock,))
-    send_msg_thread = threading.Thread(target=send_messages,args=(sock,))
+            #Inicializo hilo para recibir mensajes
+            sock.sendall(room.encode())
+            recieve_msg_thread = threading.Thread(target=receive_messages,args=(sock,))
+            send_msg_thread = threading.Thread(target=send_messages,args=(sock,))
 
-    send_msg_thread.start()
-    recieve_msg_thread.start()
-    #Esperar que ambos hilos terminen
-    recieve_msg_thread.join() 
-    send_msg_thread.join()
-
+            send_msg_thread.start()
+            recieve_msg_thread.start()
+            #Esperar que ambos hilos terminen
+            recieve_msg_thread.join() 
+            send_msg_thread.join()
+        if option == 3:
+            break
     
---
+++ menu.py
@@ -0,0 +1,18 @@
+def show_menu():
+    print("+===============================+")
+    print("|          Pychat Menu          |")
+    print("+===============================+")
+    print("| 1- P2P Chat                   |")
+    print("| 2- Join Room                  |")
+    print("| 3- Exit                       |")
+    print("+===============================+")
+
+def show_rooms():
+    print("+===============================+")
+    print("|             Rooms             |")
+    print("+===============================+")
+    print("| 1- Programmers                |")
+    print("| 2- Designers                  |")
+    print("| 3- Managers                   |")
+    print("+===============================+")
+    
