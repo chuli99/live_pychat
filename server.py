37d9d78b7d12bafd023d6910a12cd778f0d2f0c3
chuli99 <juliancastillo0399@gmail.com>
2024-04-08 12:58:53 -0300

    differents rooms in server added

--- server.py
+++ server.py
@@ -1,43 +1,54 @@
 import socketserver
 
 class ChatHandler(socketserver.BaseRequestHandler):
+    rooms = {
+        "programmers": [],
+        "designers": [],
+        "managers": []
+    }
+
     def handle(self):
         print(f"New connection from {self.client_address}")
-        self.request.sendall("Welcome to the chat server!\n".encode())
-
+        self.request.sendall("Welcome to PyChat!\n".encode())
+        option = self.request.recv(512).decode().strip()
+        room = option.lower()
+        if room not in self.rooms:
+            self.request.sendall("Invalid room. Closing connection.\n".encode())
+            return
+        self.rooms[room].append(self.request)
+        self.request.sendall(f"Welcome to {room} room!\n".encode())
+        
         while True:
-            print(self.request)
             message = self.request.recv(512).decode().strip()
             if not message:
                 break
             print(f"Received from {self.client_address}: {message}")
 
-            # Broadcast para enviar mensajes a todos los clientes:
-            for client in self.server.clients:
-                #Ver cuantos clientes hay en linea.
-                #print(len(self.server.clients))
+            # Broadcast para enviar mensajes a todos los clientes en la misma sala:
+            for client in self.rooms[room]:
                 try:
                     if client != self.request:
                         client.sendall(f"({self.client_address[0]}:{self.client_address[1]}): {message}".encode())
                 except:
                     client.close()
-                    self.server.clients.remove(client)
+                    self.rooms[room].remove(client)
                     print(f"Connection from {client.getpeername()} closed due to error")
 
     def setup(self):
-        self.server.clients.append(self.request)
+        pass
 
     def finish(self):
-        self.server.clients.remove(self.request)
-        print(f"Connection from {self.client_address} closed")
+        for room, clients in self.rooms.items():
+            if self.request in clients:
+                clients.remove(self.request)
+                print(f"Connection from {self.client_address} closed")
 
 class ChatServer(socketserver.ThreadingTCPServer):
     def __init__(self, server_address, handler_class):
         super().__init__(server_address, handler_class)
-        self.clients = []
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 5554
+    HOST, PORT = "localhost", 5555
     with ChatServer((HOST, PORT), ChatHandler) as server:
         print(f"Server started on {HOST}:{PORT}")
         server.serve_forever()
